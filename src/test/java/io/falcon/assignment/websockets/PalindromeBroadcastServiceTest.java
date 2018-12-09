87ee7f492e0bae8632e16186f3acb9b0eb77d653
falcon-candidate <45712619+falcon-candidate@users.noreply.github.com>
2018-12-09 14:21:18 +0100

    Dirty context in broadcast test

--- src/test/java/io/falcon/assignment/websockets/PalindromeBroadcastServiceTest.java
+++ src/test/java/io/falcon/assignment/websockets/PalindromeBroadcastServiceTest.java
@@ -3,6 +3,7 @@ package io.falcon.assignment.websockets;
 import static org.junit.Assert.assertEquals;
 
 import io.falcon.assignment.Application;
+import io.falcon.assignment.PalindromeQueryUtils;
 import io.falcon.assignment.model.PalindromeQuery;
 import java.io.IOException;
 import java.time.OffsetDateTime;
@@ -11,12 +12,13 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicReference;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.socket.WebSocketSession;
@@ -25,6 +27,7 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 public class PalindromeBroadcastServiceTest {
 
   @LocalServerPort
@@ -43,14 +46,10 @@ public class PalindromeBroadcastServiceTest {
     ListenableFuture<WebSocketSession> futureSession = client
         .doHandshake(handler, "ws://localhost:" + port + "/subscribe_ws");
     futureSession.get();
-    OffsetDateTime timestamp = OffsetDateTime.of(2016, 11, 6,
-        0, 20, 30, 1000,
-        ZoneOffset.ofHours(-5));
-    PalindromeQuery query = new PalindromeQuery("abc", timestamp);
-    broadcastService.broadcast(query);
+    broadcastService.broadcast(PalindromeQueryUtils.getValidQuery());
     String actual = completableFuture.get(42, TimeUnit.SECONDS);
     String expected = "{\"content\":\"abc\",\"timestamp\":\"2016-11-06 00:20:30-0500\"}";
     assertEquals(expected, actual);
   }
-  
+
 }
